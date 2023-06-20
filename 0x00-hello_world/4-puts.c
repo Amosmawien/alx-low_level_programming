9e42633ac62aec90f6cb0c6c7a9dd713712d174d
Amosmawien <mawienmawien84@gmail.com>
2023-06-20 13:51:30 +0300

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 /**
-*main - entry poit
-*return: always 0(Success)
-*/
+   * Description: main - Prints out a string to stdout.
+    * Return: 0 if success.
+     */
 int main(void)
 {
-<<<<<<< HEAD
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
-=======
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
 }
->>>>>>> de07a76c2c4acfacccbbe1706c6febcfe2a6cd88
