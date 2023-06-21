ddd47c25369890b1277f6c657d47f524c3c89535
Amosmawien <mawienmawien84@gmail.com>
2023-06-21 14:43:01 +0300

    d

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet - make the alphabet
- *
- * Return:void
- */
-void print_alphabet(void)
+   * main - check the code.
+    *
+     * Return: Always 0.
+      */
+int main(void)
 {
-char	ch;
-for (ch = 'a'; ch <= 'z'; ch++)
-_putchar(ch);
-_putchar('\n');
+	    print_alphabet_x10();
+	        return (0);
 }
