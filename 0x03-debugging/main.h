9d50656e5e2d041d68b761eebf5141277a493893
amosmawien <mawienmawien84@gmail.com>
2023-07-03 20:57:37 +0300

    0x07-pointers_arrays_strings/main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+#include <stdio.h>
+
+void positive_or _negative(int 1);
+
+int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
+
+
+
+#endif /* MAIN_H */
