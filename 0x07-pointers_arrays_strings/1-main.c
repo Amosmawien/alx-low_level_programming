f8a82e08b541698cbc0de4d2baf94c071ab4cbf5
Amosmawien <mawienmawien84@gmail.com>
2023-07-03 10:03:29 +0300

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-main.c
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
+   * simple_print_buffer - prints buffer in hexa
+    * @buffer: the address of memory to print
+     * @size: the size of the memory to print
+      *
+       * Return: Nothing.
+        */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
 	unsigned int i;
@@ -20,13 +20,13 @@ void simple_print_buffer(char *buffer, unsigned int size)
 			printf(" ");
 		}
 		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
 	}
-	printf("\n");
-}
-printf("0x%02x", buffer[i]);
-i++;
-}
-printf("\n");
+		        printf("\n");
 }
 
 /**
