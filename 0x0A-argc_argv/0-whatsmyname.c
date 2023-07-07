7e2d1d27ae7e2f5b688a91c75ada16e737c185dc
Amosmawien <mawienmawien84@gmail.com>
2023-07-07 16:43:46 +0300

    global

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,19 +1,16 @@
+#include "main.h"
 #include <stdio.h>
-/*
- * Task 0 in 0x0A. C - argc, argv
- * Purpose : Write a program that prints its name, followed by a new line.
- */
-
 
 /**
- * main - prints its name, followed by a new line.
- * @argc: argument count
- * @argv: argument vector
- * Return: (0) success
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc - 1]);
+	printf("%s\n", argv[argc * 0]);
+
 	return (0);
 }
