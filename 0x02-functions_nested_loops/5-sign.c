e944d9fc82c844094801adb081eec1ddc2921f2d
Amosmawien <mawienmawien84@gmail.com>
2023-07-08 11:08:29 +0300

    sd

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,32 @@
 #include "main.h"
-/**
- * print_sign - a function that prints the sign of a number
- * @n: number input
- * Return: 1 prints '+' if n > 0, 0 prints '0' if n = 0, -1 prints '-' if n < 0
+/*
+ * Task 5 in 0x02. C - Functions, nested loops
+ * Purpose : checks sing  of a number
+ * my sulotion: checking the sign by using comparison operators
  */
+
+/**
+ * print_sign - check sing  of a number
+ * @n: number to check
+ * Return: (1) and prints + if n is greater than zero
+ *       : (0) and prints 0 if n is zero
+ *	: (-1) and prints - if n is less than zero
+ *
+ */
+
 int print_sign(int n)
 {
-
-	if (n > 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-	else if (n == 0)
+	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	else
+	else if (n > 0)
 	{
-		_putchar('-');
-		return (-1);
+		_putchar('+');
+		return (1);
 	}
 
+	_putchar('-');
+	return (-1);
 }
